f88099fe7f3fbf3772b486f0a1413a9532cf1723
Unta01 <76728492+Gingg219@users.noreply.github.com>
2024-02-29 12:42:31 +0700

    docs: add migration

---
+++ database/migrations/2024_01_24_111309_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained();
+            $table->integer('parent_id')->nullable();
+            $table->string('author',200);
+            $table->text('content')->nullable();
+            $table->string('published')->default(0);
+            $table->date('published_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
